bc0508b72f84a4504f908c88808416871f103809
zckoh <zckoh96@gmail.com>
2020-06-21 22:21:14 +0100

    add basic homepage

--- website/homepage/views.py
+++ website/homepage/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage_view(request):
-    return render(request, 'base.html')
+    return render(request, 'homepage/homepage.html')
