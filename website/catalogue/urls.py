c235ed2610a84207dfd527c0fa8c89a8cd8df89d
zckoh <zckoh96@gmail.com>
2020-07-03 11:16:23 +0100

    update category link and filter (#3)

--- website/catalogue/urls.py
+++ website/catalogue/urls.py
@@ -8,8 +8,12 @@ urlpatterns = [
     path("", views.catalogue, name="catalogue")
 ]
 
+# base link to go back to catalogue with filter
+category_refinement_base_link = "?products_index%5BrefinementList%5D%5Bproduct_category%5D%5B0%5D="
+
 # Add additional urls for each product
 for product in models.toys_product_list():
+    product.update( {"category_link" : category_refinement_base_link + product['product_category']} )
     route = path(
         product['url_link'],
         views.ProductPage.as_view(extra_context=product),
@@ -18,6 +22,7 @@ for product in models.toys_product_list():
     urlpatterns.append(route)
 
 for product in models.copybooks_product_list():
+    product.update( {"category_link" : category_refinement_base_link + product['product_category']} )
     route = path(
         product['url_link'],
         views.ProductPage.as_view(extra_context=product),
@@ -26,6 +31,7 @@ for product in models.copybooks_product_list():
     urlpatterns.append(route)
 
 for product in models.pens_product_list():
+    product.update( {"category_link" : category_refinement_base_link + product['product_category']} )
     route = path(
         product['url_link'],
         views.ProductPage.as_view(extra_context=product),
