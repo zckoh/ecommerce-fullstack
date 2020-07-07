99eea109d0f1baf3e460433fb5ca684cebc34435
zckoh <zckoh96@gmail.com>
2020-07-07 21:30:58 +0100

    fix contact form

--- website/website/settings.py
+++ website/website/settings.py
@@ -97,8 +97,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
