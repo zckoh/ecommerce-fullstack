0d80b387f7047c06dd963f4712b03d660c033b7a
zckoh <zckoh96@gmail.com>
2020-07-08 17:39:58 +0100

    update font and texts

--- website/contact/forms.py
+++ website/contact/forms.py
@@ -5,10 +5,60 @@ class ContactForm(forms.Form):
     name = forms.CharField(
         max_length=100,
         label='Nom',
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "votre nom"}))
+        error_messages={
+            'required': "ce nom n'est pas valide",
+            'max_length': "le maximum nombre de charactère ne doit dépasser 100"
+        },
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "votre nom",
+                'oninvalid': "this.setCustomValidity('Cette zone du formulaire ne doit pas être laissé vide')",
+                'oninput': "setCustomValidity('')"
+            }
+        )
+    )
     email_address = forms.EmailField(
-        label='Adresse e-mail', widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': "votre adresse e-mail"}))
-    subject = forms.CharField(max_length=200, label='Sujet', widget=forms.TextInput(
-        attrs={'class': 'form-control', 'placeholder': "Sujet du courriel"}))
-    message = forms.CharField(widget=forms.Textarea(
-        attrs={'class': 'form-control'}), label='Your Message')
+        label='Adresse e-mail',
+        error_messages={
+            'required': "cette adresse mail n'est pas valide",
+            'invalid' : "cette adresse mail n'est pas valide"
+        },
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "votre adresse e-mail",
+                'oninvalid': "this.setCustomValidity('Cette zone du formulaire ne doit pas être laissé vide')",
+                'oninput': "setCustomValidity('')"
+            }
+        )
+    )
+    subject = forms.CharField(
+        max_length=200,
+        label='Sujet',
+        error_messages={
+            'required': 'le sujet du courriel est manquant',
+            'max_length': "le maximum nombre de charactère ne doit dépasser 200"
+        },
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': "Sujet du courriel",
+                'oninvalid': "this.setCustomValidity('Cette zone du formulaire ne doit pas être laissé vide')",
+                'oninput': "setCustomValidity('')"
+            }
+        )
+    )
+    message = forms.CharField(
+        label='Your Message',
+        error_messages={
+            'required': "ce message n'est pas valide"
+        },
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'oninvalid': "this.setCustomValidity('Cette zone du formulaire ne doit pas être laissé vide')",
+                'oninput': "setCustomValidity('')"
+            }
+        )
+    )
