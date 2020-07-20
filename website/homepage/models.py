5999614127b48200b076967eb92039c5ec608b46
zckoh <zckoh96@gmail.com>
2020-07-20 19:28:13 +0100

    add string return for notice model

--- website/homepage/models.py
+++ website/homepage/models.py
@@ -5,3 +5,6 @@ class Notice(models.Model):
     notice_title = models.CharField(max_length=200)
     notice_message = models.TextField()
     updated_date = models.DateTimeField('Last Updated', auto_now=True)
+
+    def __str__(self):
+        return self.notice_title
